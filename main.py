b2a6a565ac9ea4bb517a4b0eb2d697a65caee36e
HRM0 <103372533+HRM0@users.noreply.github.com>
2023-11-10 15:41:11 -0500

    practicing python

---
+++ main.py
@@ -0,0 +1,43 @@
+import requests
+
+USERNAME = "pythonstudent01"
+TOKEN = "&GsleEgE7F1$MKZ1"
+pixela_endpoint = "https://pixe.la/v1/users"
+
+user_param = {
+    "token": "&GsleEgE7F1$MKZ1",
+    "username": "pythonstudent01",
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes"
+}
+
+# response = requests.post(pixela_endpoint, json=user_param)
+# print(response.text)
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+graph_config = {
+    "id": "myfirstgraph",
+    "name": "practice graph",
+    "unit": "hours",
+    "type": "float",
+    "color": "shibafu"
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# print(response.text)
+
+GRAPHID = "myfirstgraph"
+post_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPHID}"
+
+post_config = {
+    "date": "20231110",
+    "quantity": "4",
+}
+
+response = requests.post(url=post_endpoint, json=post_config, headers=headers)
+print(response.text)
