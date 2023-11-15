7a61844e5be31602a1d7269f4456355fd97485ea
HRM0 <103372533+HRM0@users.noreply.github.com>
2023-11-15 18:28:37 -0500

    webscraper

--- main.py
+++ main.py
@@ -1,43 +1,17 @@
+from bs4 import BeautifulSoup
 import requests
 
-USERNAME = "pythonstudent01"
-TOKEN = "&GsleEgE7F1$MKZ1"
-pixela_endpoint = "https://pixe.la/v1/users"
+response = requests.get("https://news.ycombinator.com/")
+yc_web_page = response.text
 
-user_param = {
-    "token": "&GsleEgE7F1$MKZ1",
-    "username": "pythonstudent01",
-    "agreeTermsOfService": "yes",
-    "notMinor": "yes"
-}
+soup = BeautifulSoup(yc_web_page, "html.parser")
+links = soup.find_all("span", class_="titleline")
+articles = []
+for span in links:
+    if span.find("a"):
+        articles.append({
+            "title": span.find("a").getText(),
+            "href": span.find("a").get("href")
+                         })
 
-# response = requests.post(pixela_endpoint, json=user_param)
-# print(response.text)
-
-graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
-
-graph_config = {
-    "id": "myfirstgraph",
-    "name": "practice graph",
-    "unit": "hours",
-    "type": "float",
-    "color": "shibafu"
-}
-
-headers = {
-    "X-USER-TOKEN": TOKEN
-}
-
-# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
-# print(response.text)
-
-GRAPHID = "myfirstgraph"
-post_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPHID}"
-
-post_config = {
-    "date": "20231110",
-    "quantity": "4",
-}
-
-response = requests.post(url=post_endpoint, json=post_config, headers=headers)
-
+print(articles)
