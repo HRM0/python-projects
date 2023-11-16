1c822b095779fac8db60d73ad59f4ffe5b12e872
HRM0 <103372533+HRM0@users.noreply.github.com>
2023-11-16 11:24:57 -0500

    python web scraping

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
 from bs4 import BeautifulSoup
 import requests
 
-response = requests.get("https://news.ycombinator.com/")
+#dateToLookUp = input("What year would you like to travel to? (YYYY-MM-DD) ")
+dateToLookUp = "2005-07-01"
+response = requests.get(f"https://www.billboard.com/charts/hot-100/{dateToLookUp}/")
 yc_web_page = response.text
 
 soup = BeautifulSoup(yc_web_page, "html.parser")
-links = soup.find_all("span", class_="titleline")
-articles = []
-for span in links:
-    if span.find("a"):
-        articles.append({
-            "title": span.find("a").getText(),
-            "href": span.find("a").get("href")
-                         })
+betterSoup = soup.select("li ul li h3", id="title-of-a-story")
+titles = []
+for title in betterSoup:
+    titles.append(title.get_text().strip())
 
-print(articles)
+print(titles)
