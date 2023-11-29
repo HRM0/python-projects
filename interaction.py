d11eb652ad0198168c44f65994cb61264e39f970
HRM0 <103372533+HRM0@users.noreply.github.com>
2023-11-28 20:50:39 -0500

    optimizing clicks

--- interaction.py
+++ interaction.py
@@ -22,7 +22,6 @@ time_machine = driver.find_element(By.XPATH, value='//*[@id="buyTime machine"]/b
 prices = {}
 def getPrices():
     cursor = driver.find_element(By.CSS_SELECTOR, value='#buyCursor b')
-    print(int(cursor.text.split()[-1].replace(",", "")))
     grandma = driver.find_element(By.CSS_SELECTOR, value='#buyGrandma b')
     factory = driver.find_element(By.CSS_SELECTOR, value='#buyFactory b')
     mine = driver.find_element(By.CSS_SELECTOR, value='#buyMine b')
@@ -30,33 +29,38 @@ def getPrices():
     alchemy = driver.find_element(By.XPATH, value='//*[@id="buyAlchemy lab"]/b')
     portal = driver.find_element(By.CSS_SELECTOR, value='#buyPortal b')
     time_machine = driver.find_element(By.XPATH, value='//*[@id="buyTime machine"]/b')
-    prices["cursor"] = {"obj": cursor, "price": int(cursor.text.split()[-1].replace(",", ""))}
-    prices["grandma"] = {"obj": grandma, "price": int(grandma.text.split()[-1].replace(",", ""))}
-    prices["factory"] = {"obj": factory, "price": int(factory.text.split()[-1].replace(",", ""))}
-    prices["mine"] = {"obj": mine, "price": int(mine.text.split()[-1].replace(",", ""))}
-    prices["shipment"] = {"obj": shipment, "price": int(shipment.text.split()[-1].replace(",", ""))}
-    prices["alchemy"] = {"obj": alchemy, "price": int(alchemy.text.split()[-1].replace(",", ""))}
-    prices["portal"] = {"obj": portal, "price": int(portal.text.split()[-1].replace(",", ""))}
-    prices["time_machine"] = {"obj": time_machine, "price": int(time_machine.text.split()[-1].replace(",", ""))}
-    print(prices)
-    min_key = min(prices, key=lambda k: prices[k]["price"])
-    print(prices[min_key])
+    prices["cursor"] = {"obj": cursor, "price": int(cursor.text.split()[-1].replace(",", "")), "amount": 1}
+    prices["grandma"] = {"obj": grandma, "price": int(grandma.text.split()[-1].replace(",", "")), "amount": 5}
+    prices["factory"] = {"obj": factory, "price": int(factory.text.split()[-1].replace(",", "")), "amount": 20}
+    prices["mine"] = {"obj": mine, "price": int(mine.text.split()[-1].replace(",", "")), "amount": 50}
+    prices["shipment"] = {"obj": shipment, "price": int(shipment.text.split()[-1].replace(",", "")), "amount": 100}
+    prices["alchemy"] = {"obj": alchemy, "price": int(alchemy.text.split()[-1].replace(",", "")), "amount": 500}
+    prices["portal"] = {"obj": portal, "price": int(portal.text.split()[-1].replace(",", "")), "amount": 10000}
+    prices["time_machine"] = {"obj": time_machine,
+                              "price": int(time_machine.text.split()[-1].replace(",", "")),
+                              "amount": 20000}
     return prices
 
 
 def check_to_buy():
-    money = driver.find_element(By.ID, value='money')
-    cheapest_item = getPrices()
-    if int(money.text) > cheapest_item["price"]:
-        cheapest_item["obj"].click()
+    money = int(driver.find_element(By.ID, value='money').text.replace(",", ""))
+    item_to_buy = {"obj": "blank", "price": float('inf'), "amount": 1}
+    item_list = getPrices()
+    for item_name, item_info in item_list.items():
+        ratio = item_info["price"]/item_info["amount"]
+        curr_ratio = item_to_buy["price"]/item_to_buy["amount"]
+        if ratio < curr_ratio and item_info["price"] < (money*1.75):
+            item_to_buy = item_info
+    if item_to_buy["price"] < money and item_to_buy["obj"] != "blank":
+        item_to_buy["obj"].click()
 
 
-interval = .05
+interval = .01
 numberOfLoops = 0.0
 while True:
     cookie.click()
     numberOfLoops += 1
-    if numberOfLoops % 20 == 0:
+    if numberOfLoops % 50 == 0:
         check_to_buy()
     time.sleep(interval)
 
